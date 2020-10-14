de793f0d976aac27a846443b49d7a56caf493ef5
Paula <s0558576@htw-berlin.de>
2020-10-14 17:43:26 +0200

    init vue

---
+++ webapp/src/js/app.js
@@ -0,0 +1,6 @@
+import App from './components/App.vue';
+import Vue from 'vue';
+
+new Vue({
+  render: (h) => h(App)
+}).$mount('#app');
---
+++ webapp/src/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+<button> Hello World </button>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style>
+</style>

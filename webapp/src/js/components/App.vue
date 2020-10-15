31c845f4258ee739aa58cb16a124d0f376f6aa58
Paula <s0558576@htw-berlin.de>
2020-10-15 12:44:45 +0200

    add NewsItem with upvote button

--- webapp/src/js/components/App.vue
+++ webapp/src/js/components/App.vue
@@ -1,10 +1,32 @@
 <template>
-<button> Hello World </button>
+  <div>
+    <h1>News List</h1>
+    <div v-for="item in items" :key="item.title">
+      <NewsItem @updateItem="orderItems" :item="item"></NewsItem>
+    </div>
+  </div>
 </template>
 
 <script>
+import NewsItem from "./NewsItem.vue";
 export default {
-  name: 'app'
+  name: "app",
+  components: {
+    NewsItem,
+  },
+  methods: {
+    orderItems: function () {
+      console.log("@orderItems");
+    },
+  },
+  data: function () {
+    return {
+      items: [
+        { title: "VueJS", votes: 0 },
+        { title: "Hello world!", votes: 0 },
+      ],
+    };
+  },
 };
 </script>
 
---
+++ webapp/src/js/components/NewsItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{ item.title }} ({{ item.votes }})</div>
+    <button @click="upvote" id="upvote">Upvote</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  methods: {
+    upvote: function () {
+      this.item.votes++;
+      this.$emit("updateItem");
+    },
+  },
+};
+</script>
+
+<style>
+</style>

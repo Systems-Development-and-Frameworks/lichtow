ae51d3b57f6acf7ddd1ed0d4f34e22ff5bb03de8
Paula <s0558576@htw-berlin.de>
2020-10-15 13:10:50 +0200

    add remove button

--- webapp/src/js/components/NewsItem.vue
+++ webapp/src/js/components/NewsItem.vue
@@ -3,6 +3,7 @@
     <div>{{ item.title }} ({{ item.votes }})</div>
     <button @click="upvote" id="upvote">Upvote</button>
     <button @click="downvote" id="downvote">Downvote</button>
+    <button @click="remove" id="remove">Remove</button>
   </div>
 </template>
 
@@ -17,6 +18,9 @@ export default {
     downvote: function () {
       this.item.votes--;
       this.$emit("updateItem");
+    },
+    remove: function () {
+      this.$emit("removeItem", this.item);
     },
   },
 };

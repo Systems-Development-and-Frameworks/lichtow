c089f272c56636b4798b21275913b8c0d2cb3490
Paula <s0558576@htw-berlin.de>
2020-10-29 22:10:16 +0100

    improve output for tests

--- webapp/tests/unit/App.spec.js
+++ webapp/tests/unit/App.spec.js
@@ -2,24 +2,28 @@ import { shallowMount } from "@vue/test-utils";
 import App from "@/App.vue";
 
 describe("App.vue", () => {
-    it("renders a message when the item list is empty", () => {
-        const wrapper = shallowMount(App, {
-            data() {
-                return {
-                    items: [],
-                };
-            },
+    describe("empty", () => {
+        it("renders a message when the item list is empty", () => {
+            const wrapper = shallowMount(App, {
+                data() {
+                    return {
+                        items: [],
+                    };
+                },
+            });
+            expect(wrapper.find(".empty-list-message").text()).toBe("The list is empty :(");
         });
-        expect(wrapper.find(".empty-list-message").text()).toBe("The list is empty :(");
     });
-    it("does not render empty list message when item list is filled", () => {
-        const wrapper = shallowMount(App, {
-            data() {
-                return {
-                    items: [[{ id: 1, title: "VueJS", votes: 0 }]],
-                };
-            },
+    describe("not empty", () => {
+        it("does not render empty list message when item list is filled", () => {
+            const wrapper = shallowMount(App, {
+                data() {
+                    return {
+                        items: [[{ id: 1, title: "VueJS", votes: 0 }]],
+                    };
+                },
+            });
+            expect(wrapper.find(".empty-list-message").exists()).toBe(false);
         });
-        expect(wrapper.find(".empty-list-message").exists()).toBe(false);
     });
 });

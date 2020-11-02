f617e8c6555231efca4c1ba8dd045719a00a1883
Paula <s0558576@htw-berlin.de>
2020-11-02 19:18:48 +0100

    add reverse order tests and button

--- webapp/src/components/NewsList/NewsList.spec.js
+++ webapp/src/components/NewsList/NewsList.spec.js
@@ -19,34 +19,42 @@ describe("NewsList.vue", () => {
             const wrapper = shallowMount(NewsList, {
                 data() {
                     return {
-                        items: [[{ id: 1, title: "VueJS", votes: 0 }]],
+                        items: [{ id: 1, title: "VueJS", votes: 0 }],
                     };
                 },
             });
             expect(wrapper.find(".empty-list-message").exists()).toBe(false);
         });
-        describe("click 'Reverse order'", () => {
-            it("toggles between ascending and descending order", () => {
-                const wrapper = shallowMount(NewsList, {
-                    data() {
-                        return {
-                            items: [
-                                [
-                                    { id: 1, title: "VueJS", votes: 1 },
-                                    { id: 2, title: "TDD", votes: 2 },
-                                    { id: 3, title: "React", votes: 0 },
-                                ],
-                            ],
-                        };
-                    },
-                });
-                window.setTimeout(() => {
-                    expect(wrapper.vm.orderedItems).toEqual([
-                        { id: 3, title: "React", votes: 0 },
+        it("sorts items in descending order by default", () => {
+            let localThis = {
+                items: [
+                    { id: 1, title: "VueJS", votes: 1 },
+                    { id: 2, title: "TDD", votes: 4 },
+                    { id: 3, title: "React", votes: 0 },
+                ],
+                descending: true,
+            };
+            expect(NewsList.computed.orderedItems.call(localThis)).toEqual([
+                { id: 2, title: "TDD", votes: 4 },
+                { id: 1, title: "VueJS", votes: 1 },
+                { id: 3, title: "React", votes: 0 },
+            ]);
+        });
+        describe("Reverse order", () => {
+            it("switches to descending order", () => {
+                let localThis = {
+                    items: [
                         { id: 1, title: "VueJS", votes: 1 },
-                        { id: 2, title: "TDD", votes: 2 },
-                    ]);
-                }, 10);
+                        { id: 2, title: "TDD", votes: 4 },
+                        { id: 3, title: "React", votes: 0 },
+                    ],
+                    descending: false,
+                };
+                expect(NewsList.computed.orderedItems.call(localThis)).toEqual([
+                    { id: 3, title: "React", votes: 0 },
+                    { id: 1, title: "VueJS", votes: 1 },
+                    { id: 2, title: "TDD", votes: 4 },
+                ]);
             });
         });
     });

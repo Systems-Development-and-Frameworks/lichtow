8ab7e89111222af3dbf5c9a85f6c7f5f7e182152
Jonas <jonas@MacBook-Pro.fritz.box>
2020-11-04 16:56:33 +0100

    add action to storybook of NewsItem

--- webapp/src/components/NewsItem/NewsItem.stories.js
+++ webapp/src/components/NewsItem/NewsItem.stories.js
@@ -4,19 +4,22 @@ export default {
     title: "NewsItem",
     component: NewsItem,
     argTypes: {
-        item: { title: "text",
-                id: "integer",
-            votes: "integer" },
+        item: {
+            title: "text",
+            id: "integer",
+            votes: "integer"
+        },
+        removeItem: { action: "removeItem" }
     },
 };
 
 const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: { NewsItem },
-    template: "<NewsItem v-bind=\"$props\" />",
+    template: "<NewsItem @removeItem=\"removeItem(item)\" v-bind=\"$props\" />",
 });
 
 export const TestItem = Template.bind({});
 TestItem.args = {
-    item: {title: "Test", id: 1, votes: 2},
+    item: { title: "Test", id: 1, votes: 2 },
 };

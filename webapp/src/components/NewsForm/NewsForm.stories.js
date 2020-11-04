2e2f687c380c704fff33b8c22bf10db1ff7dd4c7
Jonas <jonas@MacBook-Pro.fritz.box>
2020-11-04 18:38:08 +0100

    add controll addon and story for NewsForm

---
+++ webapp/src/components/NewsForm/NewsForm.stories.js
@@ -0,0 +1,17 @@
+import NewsForm from "./NewsForm.vue";
+
+export default {
+    title: "NewsForm",
+    component: NewsForm,
+    argTypes: {
+        createItem: { action: "createItem" }
+    },
+};
+
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { NewsForm },
+    template: "<NewsForm @createItem=\"createItem\" v-bind=\"$props\" />",
+});
+
+export const TestForm = Template.bind({});

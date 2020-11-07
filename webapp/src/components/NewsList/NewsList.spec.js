fa9d130b89dfdb30a5332eebec437f2f50dcf390
Paula <s0558576@htw-berlin.de>
2020-11-07 11:43:03 +0100

    update tests to include ascending and descending order tests based on rendered NewsItem components

--- webapp/src/components/NewsList/NewsList.spec.js
+++ webapp/src/components/NewsList/NewsList.spec.js
@@ -1,5 +1,22 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import NewsList from "./NewsList.vue";
+import NewsItem from "../NewsItem/NewsItem.vue";
+
+const testItems = [
+    { id: 1, title: "VueJS", votes: 1 },
+    { id: 2, title: "TDD", votes: 4 },
+    { id: 3, title: "React", votes: 0 },
+];
+const descendingTestItems = [
+    { id: 2, title: "TDD", votes: 4 },
+    { id: 1, title: "VueJS", votes: 1 },
+    { id: 3, title: "React", votes: 0 },
+];
+const ascendingTestItems = [
+    { id: 3, title: "React", votes: 0 },
+    { id: 1, title: "VueJS", votes: 1 },
+    { id: 2, title: "TDD", votes: 4 },
+];
 
 describe("NewsList.vue", () => {
     describe("empty", () => {
@@ -11,50 +28,53 @@ describe("NewsList.vue", () => {
                     };
                 },
             });
-            expect(wrapper.find(".empty-list-message").text()).toBe("The list is empty :(");
+            expect(wrapper.find("#emptyListMessage").text()).toBe("The list is empty :(");
         });
     });
     describe("not empty", () => {
-        it("does not render empty list message when item list is filled", () => {
-            const wrapper = shallowMount(NewsList, {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = mount(NewsList, {
                 data() {
                     return {
-                        items: [{ id: 1, title: "VueJS", votes: 0 }],
+                        items: testItems,
                     };
                 },
             });
-            expect(wrapper.find(".empty-list-message").exists()).toBe(false);
         });
-        it("sorts items in descending order by default", () => {
+        it("does not render empty list message when item list is filled", () => {
+            expect(wrapper.find("#emptyListMessage").exists()).toBe(false);
+        });
+        it("orderedItems sorts items in descending order by default", () => {
             let localThis = {
-                items: [
-                    { id: 1, title: "VueJS", votes: 1 },
-                    { id: 2, title: "TDD", votes: 4 },
-                    { id: 3, title: "React", votes: 0 },
-                ],
+                items: testItems,
                 descending: true,
             };
-            expect(NewsList.computed.orderedItems.call(localThis)).toEqual([
-                { id: 2, title: "TDD", votes: 4 },
-                { id: 1, title: "VueJS", votes: 1 },
-                { id: 3, title: "React", votes: 0 },
-            ]);
+            expect(NewsList.computed.orderedItems.call(localThis)).toEqual(descendingTestItems);
+        });
+        it("renders items in descending order by default", () => {
+            let newsItems = wrapper.findAllComponents(NewsItem);
+            for (let i = 0; i < newsItems.length; i++) {
+                expect(newsItems.at(i).props("item").title).toEqual(descendingTestItems[i].title);
+            }
         });
         describe("Reverse order", () => {
-            it("switches to descending order", () => {
+            it("orderedItems sorts items in ascending order", () => {
                 let localThis = {
-                    items: [
-                        { id: 1, title: "VueJS", votes: 1 },
-                        { id: 2, title: "TDD", votes: 4 },
-                        { id: 3, title: "React", votes: 0 },
-                    ],
+                    items: testItems,
                     descending: false,
                 };
-                expect(NewsList.computed.orderedItems.call(localThis)).toEqual([
-                    { id: 3, title: "React", votes: 0 },
-                    { id: 1, title: "VueJS", votes: 1 },
-                    { id: 2, title: "TDD", votes: 4 },
-                ]);
+                expect(NewsList.computed.orderedItems.call(localThis)).toEqual(ascendingTestItems);
+            });
+            describe("click 'Reverse Order'", () => {
+                it("renders items in ascending order", async () => {
+                    let reverseOrderButton = wrapper.find("#reverseOrder");
+                    await reverseOrderButton.trigger("click");
+                    let newsItems = wrapper.findAllComponents(NewsItem);
+                    for (let i = 0; i < newsItems.length; i++) {
+                        expect(newsItems.at(i).props("item").title).toEqual(ascendingTestItems[i].title);
+                    }
+                });
             });
         });
     });

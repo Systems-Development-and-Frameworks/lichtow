59d4d58c0119f72e1f2f15f3446579ceeebb7d56
Jonas <jonas@MacBook-Pro.fritz.box>
2020-11-12 12:16:11 +0100

    reorganize file structure

---
+++ backend/src/posts/resolvers.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const books = [
+    {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J.K. Rowling"
+    },
+    {
+        title: "Jurassic Park",
+        author: "Michael Crichton"
+    },
+];
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+  module.exports = {
+    resolvers
+};

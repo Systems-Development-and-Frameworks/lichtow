b78a81da71c526dddb19524d5593ea551c66ccd2
Paula <s0558576@htw-berlin.de>
2020-11-12 13:04:11 +0100

    alter tests for custom data source

--- backend/src/posts/posts.spec.js
+++ backend/src/posts/posts.spec.js
@@ -2,24 +2,34 @@ const { ApolloServer, gql } = require("apollo-server");
 const { createTestClient } = require("apollo-server-testing");
 const { typeDefs } = require("./typeDefs");
 const { resolvers } = require("./resolvers");
+const { PostDataSource } = require("./datasource");
 
-const GET_BOOKS = gql`
+const dataSources = {
+    postDatasource: new PostDataSource(),
+};
+
+
+const GET_POSTS = gql`
     {
-        books {
-            author
+        posts {
+            id
+            votes
             title
         }
     }
 `;
 
-it("fetches books", async () => {
+it("fetches posts", async () => {
     const server = new ApolloServer({
         typeDefs: [typeDefs],
         resolvers: [resolvers],
+        dataSources: () => {
+            return dataSources;
+        },
     });
 
     const { query } = createTestClient(server);
 
-    const res = await query({ query: GET_BOOKS });
+    const res = await query({ query: GET_POSTS });
     expect(res).toMatchSnapshot();
 });

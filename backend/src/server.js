a43b06e1c2a885ad3823a4c275f216d161b5453b
Paula <s0558576@htw-berlin.de>
2020-11-28 16:06:38 +0100

    user graphql shields and middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,33 @@
 import { ApolloServer, gql } from "apollo-server";
+import { applyMiddleware } from "graphql-middleware";
+import { makeExecutableSchema } from "graphql-tools";
+import jwt from "jsonwebtoken";
+import { InMemoryDataSource, Post, User } from "./datasource";
+import { permissions } from "./permissions";
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
-import { InMemoryDataSource, Post, User } from "./datasource";
 
 const db = new InMemoryDataSource();
 db.posts.push(new Post({ title: "Test Post", authorName: "Jonas" }));
 
 const dataSources = () => ({ db });
 
-const context = ({ req, res }) => ({ req, res });
+const context = ({ req }) => {
+    try {
+        const { authorization } = req.headers;
+        const token = authorization.replace("Bearer ", "");
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        return { user: payload };
+    } catch (e) {
+        return { user: null };
+    }
+};
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 export default class Server {
     constructor(opts) {
         const defaults = {
-            typeDefs,
-            resolvers,
+            schema: applyMiddleware(schema, permissions),
             dataSources,
             context,
         };

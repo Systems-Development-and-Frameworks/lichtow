7038bec04f996d22734bfe6755bc73f49b05c73f
Paula <s0558576@htw-berlin.de>
2020-12-08 14:02:30 +0100

    use allow from graphql-shield

--- backend/src/permissions.js
+++ backend/src/permissions.js
@@ -1,9 +1,8 @@
-import { rule, shield } from "graphql-shield";
+import { allow, rule, shield } from "graphql-shield";
 
 const isAuthenticated = rule({ cache: "contextual" })(async (parent, args, ctx, info) => {
     return ctx.userId !== null;
 });
-const allow = rule({ cache: "contextual" })(async (parent, args, ctx, info) => true);
 
 export const permissions = shield(
     {

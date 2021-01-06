b331f82412e2407f44573a2afa1159b444a7a790
powlaa <s0558576@htw-berlin.de>
2021-01-06 16:08:15 +0100

    Use less transactions for seeding the database

--- backend/src/db/seed.js
+++ backend/src/db/seed.js
@@ -6,13 +6,11 @@ const seed = async () => {
     const session = driver.session();
     //Users
     const jonas = await User.build("Jonas", "jonas@jonas.com", "Jonas1234");
-    await session.writeTransaction((tx) =>
-        tx.run("CREATE (a:User {name: $name, email: $email, id: $id, password: $password})", jonas)
-    );
     const paula = await User.build("Paula", "paula@paula.com", "Paula1234");
-    await session.writeTransaction((tx) =>
-        tx.run("CREATE (a:User {name: $name, email: $email, id: $id, password: $password})", paula)
-    );
+    await session.writeTransaction((tx) => {
+        tx.run("CREATE (a:User {name: $name, email: $email, id: $id, password: $password})", jonas);
+        tx.run("CREATE (a:User {name: $name, email: $email, id: $id, password: $password})", paula);
+    });
 
     //Posts and Authors
     const posts = [
@@ -21,74 +19,58 @@ const seed = async () => {
         new Post("Awesome Post"),
         new Post("Yet another post"),
     ];
-    await session.writeTransaction((tx) =>
+    await session.writeTransaction((tx) => {
         tx.run("MATCH (u: User {id: $userId}) CREATE (u)-[:WROTE]->(a:Post {title: $title, id: $id})", {
             ...posts[0],
             userId: jonas.id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}) CREATE (u)-[:WROTE]->(a:Post {title: $title, id: $id})", {
             ...posts[1],
             userId: jonas.id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}) CREATE (u)-[:WROTE]->(a:Post {title: $title, id: $id})", {
             ...posts[2],
             userId: paula.id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}) CREATE (u)-[:WROTE]->(a:Post {title: $title, id: $id})", {
             ...posts[3],
             userId: paula.id,
-        })
-    );
+        });
+    });
 
     //Votes
-    await session.writeTransaction((tx) =>
+    await session.writeTransaction((tx) => {
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: 1,
             userId: paula.id,
             postId: posts[0].id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: -1,
             userId: paula.id,
             postId: posts[1].id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: 1,
             userId: paula.id,
             postId: posts[2].id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: 1,
             userId: jonas.id,
             postId: posts[0].id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: -1,
             userId: jonas.id,
             postId: posts[2].id,
-        })
-    );
-    await session.writeTransaction((tx) =>
+        });
         tx.run("MATCH (u: User {id: $userId}), (p: Post {id: $postId}) CREATE (u)-[:VOTED {value: $value}]->(p)", {
             value: 1,
             userId: jonas.id,
             postId: posts[3].id,
-        })
-    );
+        });
+    });
 };
 
 (async () => {

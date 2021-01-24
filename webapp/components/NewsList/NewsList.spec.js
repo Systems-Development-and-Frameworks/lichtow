0fe127a483e2a2ecc27c59adcfbe9e6c0a9e1192
powlaa <s0558576@htw-berlin.de>
2021-01-24 21:13:21 +0100

    Fix Tests

--- webapp/components/NewsList/NewsList.spec.js
+++ webapp/components/NewsList/NewsList.spec.js
@@ -1,75 +1,130 @@
-import { shallowMount, mount } from "@vue/test-utils";
+import { createLocalVue, mount } from "@vue/test-utils";
+import VueApollo from "vue-apollo";
+import Vuex from "vuex";
+import { createMockClient } from "mock-apollo-client";
 import NewsList from "./NewsList.vue";
 import NewsItem from "../NewsItem/NewsItem.vue";
+import allPostsQuery from "../../gql/Posts.gql";
 
-const testItems = [
-    { id: 1, title: "VueJS", votes: 1 },
-    { id: 2, title: "TDD", votes: 4 },
-    { id: 3, title: "React", votes: 0 },
-];
-const descendingTestItems = [
-    { id: 2, title: "TDD", votes: 4 },
-    { id: 1, title: "VueJS", votes: 1 },
-    { id: 3, title: "React", votes: 0 },
-];
-const ascendingTestItems = [
-    { id: 3, title: "React", votes: 0 },
-    { id: 1, title: "VueJS", votes: 1 },
-    { id: 2, title: "TDD", votes: 4 },
-];
+const postListMock = {
+    data: {
+        posts: [
+            {
+                id: "1",
+                title: "Vue",
+                votes: 4,
+                author: {
+                    id: "1",
+                    __typename: "User",
+                },
+                __typename: "Post",
+            },
+            {
+                id: "2",
+                title: "React",
+                votes: 0,
+                author: {
+                    id: "2",
+                    __typename: "User",
+                },
+                __typename: "Post",
+            },
+            {
+                id: "3",
+                title: "TDD",
+                votes: 2,
+                author: {
+                    __typename: "User",
+                    id: "3",
+                },
+                __typename: "Post",
+            },
+        ],
+    },
+};
+
+const localVue = createLocalVue();
+localVue.use(VueApollo);
+localVue.use(Vuex);
 
 describe("NewsList.vue", () => {
-    describe("empty", () => {
-        it("renders a message when the item list is empty", () => {
-            const wrapper = shallowMount(NewsList, {
-                propsData: {
-                    initialItems: [],
+    let wrapper;
+    let mockClient;
+    let apolloProvider;
+    let requestHandlers;
+    let store = new Vuex.Store({
+        getters: {
+            loggedIn: () => false,
+        },
+        mutations: {
+            setToken(token, state) {},
+        },
+    });
+
+    const createComponent = (handlers) => {
+        mockClient = createMockClient({
+            resolvers: {},
+        });
+        requestHandlers = {
+            allPostsQueryHandler: jest.fn().mockResolvedValue({ ...postListMock }),
+            ...handlers,
+        };
+        mockClient.setRequestHandler(allPostsQuery, requestHandlers.allPostsQueryHandler);
+        apolloProvider = new VueApollo({ defaultClient: mockClient });
+        const getToken = jest.fn();
+        wrapper = mount(NewsList, {
+            store,
+            localVue,
+            apolloProvider,
+            mocks: {
+                $apolloHelpers: {
+                    getToken,
                 },
+            },
+        });
+    };
+
+    afterEach(() => {
+        wrapper.destroy();
+        mockClient = null;
+        apolloProvider = null;
+    });
+
+    it("renders a Vue component", () => {
+        createComponent();
+        expect(wrapper.exists()).toBe(true);
+        expect(wrapper.vm.$apollo.queries.items).toBeTruthy();
+    });
+
+    describe("empty", () => {
+        it("renders a message when the item list is empty", async () => {
+            createComponent({
+                allPostsQueryHandler: jest.fn().mockResolvedValue({ data: { posts: [] } }),
             });
+            await wrapper.vm.$nextTick();
+
             expect(wrapper.find("#emptyListMessage").text()).toBe("The list is empty :(");
         });
     });
     describe("not empty", () => {
-        let wrapper;
-        beforeEach(() => {
-            wrapper = mount(NewsList, {
-                propsData: {
-                    initialItems: testItems,
-                },
-            });
+        beforeEach(async () => {
+            createComponent();
+            await wrapper.vm.$nextTick();
         });
         it("does not render empty list message when item list is filled", () => {
             expect(wrapper.find("#emptyListMessage").exists()).toBe(false);
         });
-        it("orderedItems sorts items in descending order by default", () => {
-            let localThis = {
-                items: testItems,
-                descending: true,
-            };
-            expect(NewsList.computed.orderedItems.call(localThis)).toEqual(descendingTestItems);
-        });
         it("renders items in descending order by default", () => {
             let newsItems = wrapper.findAllComponents(NewsItem);
-            expect(newsItems.wrappers.map((i) => i.props("item").title)).toEqual(
-                descendingTestItems.map((i) => i.title)
-            );
+            expect(newsItems.wrappers.map((i) => i.props("item").title)).toEqual(["Vue", "TDD", "React"]);
         });
         describe("Reverse order", () => {
-            it("orderedItems sorts items in ascending order", () => {
-                let localThis = {
-                    items: testItems,
-                    descending: false,
-                };
-                expect(NewsList.computed.orderedItems.call(localThis)).toEqual(ascendingTestItems);
-            });
             describe("click 'Reverse Order'", () => {
                 it("renders items in ascending order", async () => {
                     let reverseOrderButton = wrapper.find("#reverseOrder");
                     await reverseOrderButton.trigger("click");
                     let newsItems = wrapper.findAllComponents(NewsItem);
-                    expect(newsItems.wrappers.map((i) => i.props("item").title)).toEqual(
-                        ascendingTestItems.map((i) => i.title)
-                    );
+                    expect(newsItems.wrappers.map((i) => i.props("item").title)).toEqual(["React", "TDD", "Vue"]);
                 });
             });
         });

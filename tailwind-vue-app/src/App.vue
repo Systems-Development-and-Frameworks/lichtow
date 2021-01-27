8c5f077a5ab29d94b50f41233a0c536310440690
Paula <s0558576@htw-berlin.de>
2021-01-28 00:07:03 +0100

    Add Tailwind vue app

---
+++ tailwind-vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="flex flex-col justify-between w-screen h-screen">
+      <Header />
+      <ContentSection class="flex-grow" />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import ContentSection from "./components/ContentSection.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    ContentSection,
+    Header,
+    Footer,
+  },
+};
+</script>
